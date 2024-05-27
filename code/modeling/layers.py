3a2d7cb381454eea6998a7481b4d90106a91d4a8
queziaa <queziaa31@gmail.com>
2024-05-28 01:52:57 +0800

    1

--- code/modeling/layers.py
+++ code/modeling/layers.py
@@ -3,17 +3,20 @@ import tensorflow as tf
 import numpy as np
 from .utils import get_shape
 
+# 尝试从tensorflow.contrib.rnn导入LSTMStateTuple，如果失败，则从tf.nn.rnn_cell导入
 try:
   from tensorflow.contrib.rnn import LSTMStateTuple
 except ImportError:
   LSTMStateTuple = tf.nn.rnn_cell.LSTMStateTuple
 
-
-
+# 定义双向RNN函数
 def bidirectional_rnn(cell_fw, cell_bw, inputs, input_lengths,
                       initial_state_fw=None, initial_state_bw=None,
                       scope=None):
+  # 创建一个变量作用域
   with tf.variable_scope(scope or 'bi_rnn') as scope:
+    # 使用tf.nn.bidirectional_dynamic_rnn创建一个双向RNN
+    # 它会返回前向和后向的输出以及状态
     (fw_outputs, bw_outputs), (fw_state, bw_state) = tf.nn.bidirectional_dynamic_rnn(
       cell_fw=cell_fw,
       cell_bw=cell_bw,
@@ -24,9 +27,12 @@ def bidirectional_rnn(cell_fw, cell_bw, inputs, input_lengths,
       dtype=tf.float32,
       scope=scope
     )
+    # 将前向和后向的输出沿着第二个维度（axis=2）拼接起来
     outputs = tf.concat((fw_outputs, bw_outputs), axis=2)
 
+    # 定义一个函数，用于拼接前向和后向的状态
     def concatenate_state(fw_state, bw_state):
+      # 如果状态是LSTMStateTuple类型，那么分别拼接其c和h属性
       if isinstance(fw_state, LSTMStateTuple):
         state_c = tf.concat(
           (fw_state.c, bw_state.c), 1, name='bidirectional_concat_c')
@@ -34,10 +40,12 @@ def bidirectional_rnn(cell_fw, cell_bw, inputs, input_lengths,
           (fw_state.h, bw_state.h), 1, name='bidirectional_concat_h')
         state = LSTMStateTuple(c=state_c, h=state_h)
         return state
+      # 如果状态是tf.Tensor类型，那么直接拼接
       elif isinstance(fw_state, tf.Tensor):
         state = tf.concat((fw_state, bw_state), 1,
                           name='bidirectional_concat')
         return state
+      # 如果状态是元组类型，且前向和后向状态的长度相同，那么逐个拼接它们的元素
       elif (isinstance(fw_state, tuple) and
             isinstance(bw_state, tuple) and
             len(fw_state) == len(bw_state)):
@@ -45,27 +53,37 @@ def bidirectional_rnn(cell_fw, cell_bw, inputs, input_lengths,
         state = tuple(concatenate_state(fw, bw)
                       for fw, bw in zip(fw_state, bw_state))
         return state
-
+      # 如果状态的类型未知，那么抛出一个错误
       else:
         raise ValueError(
           'unknown state type: {}'.format((fw_state, bw_state)))
 
+    # 拼接前向和后向的状态
     state = concatenate_state(fw_state, bw_state)
+    # 返回输出和状态
     return outputs, state
 
 def masking(scores, sequence_lengths, score_mask_value=tf.constant(-np.inf)):
+  # 创建一个掩码，形状为 [batch_size, sequence_length]
   score_mask = tf.sequence_mask(sequence_lengths, maxlen=tf.shape(scores)[1])
+  # 创建一个掩码值张量，形状和scores相同
   score_mask_values = score_mask_value * tf.ones_like(scores)
+  # 使用掩码选择scores或score_mask_values中的元素
   return tf.where(score_mask, scores, score_mask_values)
 
 def partial_softmax(logits, weights, dim, name,):
   with tf.variable_scope('partial_softmax'):
+    # 计算logits的指数
     exp_logits = tf.exp(logits)
+    # 如果logits和weights的形状相同，则直接相乘
+    # 否则，先扩展weights的维度，然后再相乘
     if len(exp_logits.get_shape()) == len(weights.get_shape()):
       exp_logits_weighted = tf.multiply(exp_logits, weights)
     else:
       exp_logits_weighted = tf.multiply(exp_logits, tf.expand_dims(weights, -1))
+    # 计算加权logits的和
     exp_logits_sum = tf.reduce_sum(exp_logits_weighted, axis=dim, keep_dims=True)
+    # 计算部分softmax得分
     partial_softmax_score = tf.div(exp_logits_weighted, exp_logits_sum, name=name)
     return partial_softmax_score
 
@@ -73,71 +91,101 @@ def attention(inputs, att_dim, sequence_lengths, scope=None):
   assert len(inputs.get_shape()) == 3 and inputs.get_shape()[-1].value is not None
 
   with tf.variable_scope(scope or 'attention'):
+    # 创建一个注意力权重变量，形状为 [att_dim, 1]
     word_att_W = tf.get_variable(name='att_W', shape=[att_dim, 1])
 
+    # 对输入进行投影，形状为 [batch_size, sequence_length, att_dim]
     projection = tf.layers.dense(inputs, att_dim, tf.nn.tanh, name='projection')
 
+    # 计算alpha，形状为 [batch_size, sequence_length]
     alpha = tf.matmul(tf.reshape(projection, shape=[-1, att_dim]), word_att_W)
     alpha = tf.reshape(alpha, shape=[-1, get_shape(inputs)[1]])
     alpha = masking(alpha, sequence_lengths, tf.constant(-1e15, dtype=tf.float32))
     alpha = tf.nn.softmax(alpha)
 
+    # 计算输出，形状为 [batch_size, embedding_dim]
     outputs = tf.reduce_sum(inputs * tf.expand_dims(alpha, 2), axis=1)
     return outputs, alpha
-
 
 def attention_pre(inputs, sequence_lengths, pre_att, scope=None):
   assert len(inputs.get_shape()) == 3 and inputs.get_shape()[-1].value is not None
 
   with tf.variable_scope(scope or 'attention'):
-    # [B*s, e] = [B,e]
+    # 创建一个注意力权重变量，形状为 [batch_size, sequence_length, embedding_dim]
     word_att_W = tf.tile(pre_att, [1, get_shape(inputs)[1]])
-    # [B,s,e] = [B*s, e]
     word_att_W = tf.reshape(word_att_W, [-1, get_shape(inputs)[1], get_shape(pre_att)[1]])
-    # [B,s,e]=[B,s,e][B,s,e]
+
+    # 计算alpha，形状为 [batch_size, sequence_length]
     alpha = tf.multiply(inputs, word_att_W)
-    # [B,s] = [B,s,e]
     alpha = tf.reduce_sum(alpha,-1)
     alpha = masking(alpha, sequence_lengths, tf.constant(-1e15, dtype=tf.float32))
-    # [B,s]
     alpha = tf.nn.softmax(alpha)
-    # [B,e] = [B,s, e][B,s,1]
+
+    # 计算输出，形状为 [batch_size, embedding_dim]
     outputs = tf.reduce_sum(inputs * tf.expand_dims(alpha, 2), axis=1)
     return outputs, alpha
 
 def attention_class(inputs, class_embedding, input_mask, sequence_lengths, class_num, class_embedding_dim, scope=None):
+  # 确保输入的形状是3D的，且最后一维有值
   assert len(inputs.get_shape()) == 3 and inputs.get_shape()[-1].value is not None
 
   with tf.variable_scope(scope or 'attention_class'):
-    x_mask = tf.expand_dims(input_mask, axis=-1)  # b*s*1
-    x_emb_0 = inputs # b * s * e
-    x_emb_1 = tf.multiply(x_emb_0, x_mask)  # b * s * e
+    # 扩展输入掩码的维度，形状变为 [batch_size, sequence_length, 1]
+    x_mask = tf.expand_dims(input_mask, axis=-1)
+    # 输入张量，形状为 [batch_size, sequence_length, embedding_dim]
+    x_emb_0 = inputs
+    # 将输入张量和掩码相乘，形状为 [batch_size, sequence_length, embedding_dim]
+    x_emb_1 = tf.multiply(x_emb_0, x_mask)
 
-    x_emb_norm = tf.nn.l2_normalize(x_emb_1, dim=2)  # b * s * e
-    W_class_tran = tf.transpose(class_embedding, [1, 0])  # e * c
-    W_class_norm = tf.nn.l2_normalize(W_class_tran, dim=0)  # e * c
-    G = tf.contrib.keras.backend.dot(x_emb_norm, W_class_norm)  # b * s * c
+    # 对输入张量进行L2标准化，形状为 [batch_size, sequence_length, embedding_dim]
+    x_emb_norm = tf.nn.l2_normalize(x_emb_1, dim=2)
+    # 转置类别嵌入，形状为 [embedding_dim, class_num]
+    W_class_tran = tf.transpose(class_embedding, [1, 0])
+    # 对转置后的类别嵌入进行L2标准化，形状为 [embedding_dim, class_num]
+    W_class_norm = tf.nn.l2_normalize(W_class_tran, dim=0)
+    # 计算输入张量和类别嵌入的点积，形状为 [batch_size, sequence_length, class_num]
+    G = tf.contrib.keras.backend.dot(x_emb_norm, W_class_norm)
+
+    # 定义卷积核的形状
     filter_shape = [3, class_num, class_num]
+    # 创建卷积核权重变量W，形状为 [3, class_num, class_num]
     W = tf.get_variable(name='W', shape=filter_shape,
                         initializer=tf.truncated_normal_initializer(stddev=0.1),
                         dtype=tf.float32, trainable=True)
+    # 创建偏置变量b，形状为 [class_num]
     b = tf.get_variable(name='b', shape=[class_num],
                         initializer=tf.constant_initializer(0.1),
                         dtype=tf.float32, trainable=True)
-    print('Scope %s with LEAM module' % scope)
-    # [B, seq, class_num]
+
+    # 执行卷积操作，形状为 [batch_size, sequence_length, class_num]
     conv = tf.nn.conv1d(G, W, stride=1, padding='SAME', name='conv')
+    # 添加偏置，形状为 [batch_size, sequence_length, class_num]
     Att_v_1 = tf.nn.bias_add(conv, b)
-    # [B, seq]
+    # 对最后一维求最大值，形状为 [batch_size, sequence_length]
     Att_v = tf.reduce_max(Att_v_1, axis=-1)
-    # [B, seq]
-    Att_v_max = masking(Att_v, sequence_lengths, tf.constant(-1e15, dtype=tf.float32))  # b * s
-    # [B, seq]
+
+    # 对注意力值进行掩码操作，形状为 [batch_size, sequence_length]
+    Att_v_max = masking(Att_v, sequence_lengths, tf.constant(-1e15, dtype=tf.float32))
+    # 对注意力值进行softmax操作，形状为 [batch_size, sequence_length]
     Att_v_max = tf.nn.softmax(Att_v_max)
-    # [B, seq, 1] [b, s ,1]
+
+    # 将注意力值与输入掩码相乘，形状为 [batch_size, sequence_length, 1]
     Att_v_max = tf.multiply(tf.expand_dims(Att_v_max, -1), x_mask)
-    #  [b * s * e] [B, seq, 1]
+    # 将输入张量与注意力值相乘，形状为 [batch_size, sequence_length, embedding_dim]
     x_att = tf.multiply(x_emb_0, Att_v_max)
-    # [B, e]
+    # 对最后一维求和，形状为 [batch_size, embedding_dim]
     outputs = tf.reduce_sum(x_att, axis=1)
+    # 返回输出和注意力值
     return outputs, Att_v_max
+
+# 这个函数attention_class是一个用于计算类别注意力的函数，它的输入包括输入向量、类别嵌入、输入掩码、序列长度、类别数量和类别嵌入维度。函数的主要步骤如下：
+
+# 首先，函数通过扩展输入掩码和输入向量的元素级乘法来应用输入掩码。
+# 然后，函数对结果进行L2标准化，并对类别嵌入进行转置和L2标准化。
+# 接着，函数计算输入向量和类别嵌入的点积，得到一个新的张量G。
+# 函数创建一个卷积层，其权重和偏置由可训练的变量W和b表示。
+# 使用G作为输入，通过卷积层进行卷积操作，然后添加偏置，得到Att_v_1。
+# 对Att_v_1沿着最后一个维度取最大值，得到Att_v。
+# 使用masking函数对Att_v进行掩码操作，然后应用softmax函数，得到Att_v_max。
+# 将Att_v_max与输入掩码进行元素级乘法，然后与原始输入向量进行元素级乘法，得到x_att。
+# 最后，沿着第一个维度对x_att进行求和，得到输出向量。
--- code/modeling/utils.py
+++ code/modeling/utils.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import numpy as np
 import pickle
 
-
+# 获取张量的形状
 def get_shape(tensor):
   static_shape = tensor.shape.as_list()
   dynamic_shape = tf.unstack(tf.shape(tensor))
@@ -10,7 +10,7 @@ def get_shape(tensor):
           for s in zip(static_shape, dynamic_shape)]
   return dims
 
-
+# 计算训练变量的总参数数量
 def count_parameters(trained_vars):
   total_parameters = 0
   print('=' * 100)
@@ -25,7 +25,7 @@ def count_parameters(trained_vars):
   print("Total trainable parameters: %d" % total_parameters)
   print('=' * 100)
 
-
+# 读取词汇表
 def read_vocab(vocab_file):
   print('Loading vocabulary ...')
   with open(vocab_file, 'rb') as f:
@@ -33,7 +33,7 @@ def read_vocab(vocab_file):
     print('Vocabulary size = %d' % len(word_to_index))
     return word_to_index
 
-
+# 批量文档标准化
 def batch_doc_normalize(docs):
   sent_lengths = np.array([len(doc) for doc in docs], dtype=np.int32)
   max_sent_length = sent_lengths.max()
@@ -50,28 +50,48 @@ def batch_doc_normalize(docs):
 
   return padded_docs, sent_lengths, max_sent_length, word_lengths, max_word_length
 
-
+# 加载Glove预训练词嵌入
 def load_glove(glove_file, emb_size, vocab):
+  # 打印加载Glove预训练词嵌入的消息
   print('Loading Glove pre-trained word embeddings ...')
+  # 初始化词嵌入权重字典
   embedding_weights = {}
+  # 打开Glove文件
   f = open(glove_file, encoding='utf-8')
+  # 遍历Glove文件中的每一行
   for line in f:
+    # 分割行中的值
     values = line.split()
+    # 获取单词
     word = values[0]
+    # 获取单词对应的向量，并将其转换为浮点类型的numpy数组
     vector = np.asarray(values[1:], dtype='float32')
+    # 将单词和对应的向量添加到词嵌入权重字典中
     embedding_weights[word] = vector
+  # 关闭Glove文件
   f.close()
+  # 打印词嵌入权重字典的长度和Glove文件的路径
   print('Total {} word vectors in {}'.format(len(embedding_weights), glove_file))
 
+  # 初始化词嵌入矩阵，其形状为(词汇表长度, 嵌入大小)，并且值在-0.5/emb_size到0.5/emb_size之间均匀分布
   embedding_matrix = np.random.uniform(-0.5, 0.5, (len(vocab), emb_size)) / emb_size
 
+  # 初始化未在词嵌入权重字典中找到的单词数量
   oov_count = 0
+  # 遍历词汇表中的每一个单词和对应的索引
   for word, i in vocab.items():
+    # 获取单词对应的词嵌入向量
     embedding_vector = embedding_weights.get(word)
+    # 如果词嵌入向量存在
     if embedding_vector is not None:
+      # 将词嵌入矩阵中对应索引的行设置为词嵌入向量
       embedding_matrix[i] = embedding_vector
+    # 如果词嵌入向量不存在
     else:
+      # 未在词嵌入权重字典中找到的单词数量加1
       oov_count += 1
+  # 打印未在词嵌入权重字典中找到的单词数量
   print('Number of OOV words = %d' % oov_count)
 
+  # 返回词嵌入矩阵
   return embedding_matrix
